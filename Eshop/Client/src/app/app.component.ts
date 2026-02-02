e43ac6e50d3dca898440945ea350ad5125029703
Sarthak <sarthjain18@gmail.com>
2026-02-02 17:38:13 +0530

    End of Section 10

--- Eshop/Client/src/app/app.component.ts
+++ Eshop/Client/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ShopComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
